648f879f0ede74ab6961559fa9bf7ca8c6a41ca8
paulosestini <24728744+paulosestini@users.noreply.github.com>
2023-03-24 18:00:21 -0300

    added curriculum authorization message schema

--- messaging.tf
+++ messaging.tf
@@ -4,12 +4,28 @@ resource "google_pubsub_schema" "user_creation" {
   definition = "{\r\n \"type\" : \"record\",\r\n \"name\" : \"Avro\",\r\n \"fields\" : [\r\n   {\r\n     \"name\": \"id\",\r\n     \"type\": \"long\"\r\n   },\r\n   {\r\n     \"name\" : \"name\",\r\n     \"type\" : \"string\"\r\n   },\r\n   {\r\n     \"name\": \"email\",\r\n     \"type\": \"string\"\r\n   }\r\n ]\r\n}\r\n"
 }
 
+resource "google_pubsub_schema" "curriculum_authorization" {
+  name = "curriculum-authorization"
+  type = "AVRO"
+  definition = "{\r\n \"type\" : \"record\",\r\n \"name\" : \"Avro\",\r\n \"fields\" : [\r\n   {\r\n     \"name\" : \"action\",\r\n     \"type\" : \"string\"\r\n   },\r\n   {\r\n     \"name\" : \"companyId\",\r\n     \"type\" : \"string\"\r\n   },\r\n   {\r\n     \"name\" : \"studentId\",\r\n     \"type\" : \"string\"\r\n   }\r\n ]\r\n}\r\n"
+}
+
 resource "google_pubsub_topic" "user_creation" {
   name = "user-creation"
 
   depends_on = [google_pubsub_schema.user_creation]
   schema_settings {
     schema = "projects/${var.project}/schemas/user-creation"
+    encoding = "JSON"
+  }
+}
+
+resource "google_pubsub_topic" "curriculum_authorization" {
+  name = "curriculum-authorization"
+
+  depends_on = [google_pubsub_schema.curriculum_authorization]
+  schema_settings {
+    schema = "projects/${var.project}/schemas/curriculum-authorization"
     encoding = "JSON"
   }
 }
